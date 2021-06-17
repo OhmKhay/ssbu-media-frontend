4f634433a06c298f54b22153583ed3c82273bc7d
OhmKhay <chestersai12@gmail.com>
2021-06-17 17:49:00 +0630

    added fe

--- pages/album/[slug].js
+++ pages/album/[slug].js
@@ -4,16 +4,18 @@ import Album from "../../src/components/Album/Album";
 import { AlbumData } from '../../src/components/Album/AlbumData';
 import Layout from "../../src/components/App/Layout";
 
-const SingleAlbum = ({album}) => {
+const SingleAlbum = ({ slug }) => {
  
-
+    const album = AlbumData?.filter((data) => {
+        return data.slug === slug; 
+    });
 
     return (
         <Layout>
            <div style={{
                padding: '2rem'
            }}>
-           { <Album album={[...album]} />}
+            <Album album={album} />
            </div>
         </Layout>
     )
@@ -22,11 +24,9 @@ const SingleAlbum = ({album}) => {
 
 SingleAlbum.getInitialProps = async ({ query }) => {
     const { slug } = query
-    const MyAlbum = AlbumData.filter((data) => {
-        return data.slug === slug; 
-    });
+   
 
-    return { album: MyAlbum }
+    return { slug }
   }
 
 export default SingleAlbum;
